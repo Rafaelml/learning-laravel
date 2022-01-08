58ea43e8d07519bbc0f2c0ccea51173b5c03872e
Rafaelml <lraaelmadrigal@gmail.com>
2022-01-08 03:44:21 +0100

    Blade layouts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<title>My blog </title>
-<link rel="stylesheet" href="/app.css">
-<body>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">{{$post->title}}</a>
-            </h1>
-            <div>
-                <?=$post->excerpt;?>
-            </div>
-        </article>
-    @endforeach
-</body>
+ <x-layout>
+         @foreach ($posts as $post)
+             <article>
+                 <h1>
+                     <a href="/posts/{{$post->slug}}">{{$post->title}}</a>
+                 </h1>
+                 <div>
+                     {{$post->excerpt}}
+                 </div>
+             </article>
+         @endforeach
+ </x-layout>
