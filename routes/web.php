2a1e5045456e5780bdb24f6d9c0454efb6c57826
Rafaelml <lraaelmadrigal@gmail.com>
2022-02-18 16:25:52 +0100

    Show All Posts Associated With a Category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
     use App\Models\Post;
+    use App\Models\Category;
     use Illuminate\Support\Facades\Route;
     use Illuminate\Support\Facades\File;
 /*
@@ -21,3 +22,6 @@
     Route::get( 'posts/{post}', function ( Post $post ) {
         return view( 'post',['post' => $post ]);
     } );
+    Route::get('categories/{category:slug}', function (Category $category){
+        return view('posts',['posts' => $category->posts]);
+    });
